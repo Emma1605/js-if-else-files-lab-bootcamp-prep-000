8ad04778946d62b40dd39eddc305c4c39c3e9baa
debonair-proc-5718 <jeanemma6@gmail.com>
2019-07-12 17:40:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var name = "Joe"
 var height = "74"
 if (name==="Susan")
 {
-  consol.log("The name is correct")
-
+  console.log("The name is correct")
+} else {
+  console.log("Expected the name to be Susan")
 }
 // Don't worry about this
 module.exports = { name, height
